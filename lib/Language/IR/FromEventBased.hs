5283fa75a0d1e1017469c5912c25929ddbb7b5f8
Rohit Ramesh <Rohit507@gmail.com>
2014-11-24 17:34:54 -0500

    Preliminary definitions for all Action Expressions

--- lib/Language/IR/FromEventBased.hs
+++ lib/Language/IR/FromEventBased.hs
@@ -11,6 +11,7 @@ import qualified Data.Map as Map
 import Language.EventBased.Parser (Interval,Email,BinOp,UnOp)
 import Data.Time.LocalTime (LocalTime)
 import Control.Monad.State
+import Control.Monad.Trans (lift) 
 import Control.Lens 
 
 -- Utility Types -- 
@@ -19,8 +20,8 @@ data ID = Block String
         | Table String 
         | Event String
         | Field String 
-        | Reg String
-        | Var String 
+        | Register String
+        | Variable String 
         deriving (Show,Read,Eq,Ord)
 
 -- Types for the Transformer Monad --
@@ -56,13 +57,32 @@ makeLenses ''BlockState
 
 type BlockTransformer = StateT BlockState Transformer
 
-getNextCounterB :: BlockTransformer Int 
-getNextCounterB = lift getNextCounter
-
 -- Functions for the BlockTransformer Monad --
 
 initBlockState :: Block -> BlockState 
 initBlockState b = BlockState b 
+
+getNextCounterB :: BlockTransformer Int 
+getNextCounterB = lift getNextCounter
+
+getNextRegNameB :: BlockTransformer String
+getNextRegNameB = do c <- getNextCounterB
+                     return $ "reg_" ++ (show c) 
+
+addInstruction :: Instruction -> BlockTransformer () 
+addInstruction i = workingBlock %= (++ [i]) 
+
+-- TODO : seriously is there a better way to do this? This is ugly as fuck. 
+doWithinB :: Transformer m -> BlockTransformer m
+doWithinB t = do s <- (lift get)
+                 let (a,ns) = runState t s
+                 lift (put ns)
+                 return a
+
+-- Lifted Versions of Infix operators -- 
+--
+infix 4 ^%=
+a ^%= b = lift (a %= b)
 
 -- Converter Functions --
 
@@ -84,17 +104,17 @@ convertActAssigns = do l <- use $ ast . actAssign
                                   
 convertActAssign :: P.AAssign -> Transformer() 
 convertActAssign (P.AAssign (P.ID i) b) = 
-  do bID <- convertBlock ("act_assign_" ++ i) b
-     environment %= Map.insert i bID 
+  do (BlockID bID) <- convertBlock ("act_assign_" ++ i) b
+     environment %= Map.insert i (Block bID) 
 
-convertBlock :: String -> [P.AExpr] -> Transformer ID
+convertBlock :: String -> [P.AExpr] -> Transformer BlockID
 convertBlock s b = do let initState = initBlockState []
                       retState <- execStateT (mapM_ convertAExpr b) initState
                       c <- getNextCounter
-                      let id =  s ++ "_" ++ (show c)
+                      let id = BlockID $ s ++ "_" ++ (show c)
                       let newBlock = retState ^. workingBlock
-                      ir . blocks %= Map.insert (BlockID id) newBlock 
-                      return $ Block id
+                      ir . blocks %= Map.insert id newBlock 
+                      return id
 
 convertAExpr :: P.AExpr -> BlockTransformer ()
 convertAExpr (P.AEGather r s)  = convertAEGather r s 
@@ -105,24 +125,52 @@ convertAExpr (P.AEDo s)        = convertAEDo s
 convertAExpr (P.AEVassign s v) = convertAEVassign s v
 
 convertAEGather :: [P.Record] -> String -> BlockTransformer ()
-convertAEGather = error "unimplemented"
+convertAEGather r t = 
+  do let bt = TableID t 
+     dl <- mapM (convertRecord bt) r 
+     let (blocks,fields) = unzip dl 
+     addInstruction $ SimultAt Waiting Now blocks
+     addInstruction $ Gather bt fields
+     
+convertRecord :: TableID -> P.Record -> BlockTransformer (BlockID,(FieldID,Value))
+convertRecord t (P.Record v s) = 
+  do c <- getNextCounterB 
+     let vName = "_fld_var_" ++ s ++ (show c)
+     let bName = "record_blk_" ++ s
+     let fID   = (FieldID s) 
+     ir . tables ^%= Map.insertWith (++) t [fID]
+     bID <- doWithinB $ convertBlock bName [P.AEVassign (P.ID vName) v]
+     return (bID,(fID,Var (VarID vName)))
 
 convertAESend :: P.Email -> P.VExpr -> BlockTransformer ()
-convertAESend e v = error "Unimplemented"
+convertAESend e v = do sr <- convertVExpr v
+                       addInstruction $ Send e sr
 
 convertAEExec :: P.VExpr -> BlockTransformer ()
-convertAEExec v = error "Unimplemented"
+convertAEExec v = void $ convertVExpr v
 
 convertAEDo :: P.ID -> BlockTransformer ()
-convertAEDo (P.ID id) = error "Unimplemented"
+convertAEDo (P.ID id) = 
+  do (Just (Block bID)) <- lift $ uses environment (Map.lookup id) 
+     addInstruction $ SimultAt Waiting Now [BlockID bID]
 
 convertAEVassign :: P.ID -> P.VExpr -> BlockTransformer ()
-convertAEVassign (P.ID id) v = error "Unimplemented"
+convertAEVassign (P.ID id) v =
+  do v <- convertVExpr v
+     addInstruction $ Store (VarID id) v
 
+-- TODO : This doesn't use the Maybe Feature at all add that 
 convertAEIf :: P.VExpr -> [P.AExpr] -> [P.AExpr] -> BlockTransformer ()
-convertAEIf v bt bf = error "Unimplemented"
+convertAEIf v bt bf = 
+  do vr <- convertVExpr v 
+     c <- getNextCounterB
+     let btName = "if_true_" ++ (show c) 
+     btID <- doWithinB $ convertBlock btName bt 
+     let bfName = "if_false_" ++ (show c) ++ "_"
+     bfID <- doWithinB $ convertBlock bfName bf 
+     addInstruction $ If vr (Just btID) (Just bfID)
 
-convertVExpr :: P.VExpr -> BlockTransformer RegID 
+convertVExpr :: P.VExpr -> BlockTransformer Value
 convertVExpr v = error "Unimplemented" 
 
 convertValAssigns :: Transformer ()
