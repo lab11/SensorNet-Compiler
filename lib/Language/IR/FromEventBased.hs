a8e95c29c98df1cb8a2eb39a53e8ef10c5563bdb
Rohit Ramesh <Rohit507@gmail.com>
2014-11-24 14:45:54 -0500

    Added a few more bits to converter

--- lib/Language/IR/FromEventBased.hs
+++ lib/Language/IR/FromEventBased.hs
@@ -5,12 +5,13 @@ module Language.IR.FromEventBased (
 
 import Language.IR.IR 
 import qualified Language.EventBased.Parser as P 
+import Language.EventBased.Parser (actAssign,valAssign,rules)
 import Data.Map (Map) 
 import qualified Data.Map as Map
 import Language.EventBased.Parser (Interval,Email,BinOp,UnOp)
 import Data.Time.LocalTime (LocalTime)
 import Control.Monad.State
-import Control.Lens
+import Control.Lens 
 
 -- Utility Types -- 
 
@@ -40,15 +41,23 @@ type Transformer = State TransformState
 initTransformState :: P.Program -> TransformState
 initTransformState s = TransformState s emptyProg Map.empty 0
 
+getNextCounter :: Transformer Int 
+getNextCounter = do i <- use counter
+                    counter += 1 
+                    return i 
+
 -- Types for the BlockTransformer Monad --
 
 data BlockState = BlockState {
-  _currentBlock :: Block
+  _workingBlock :: Block
 }
 
 makeLenses ''BlockState
 
 type BlockTransformer = StateT BlockState Transformer
+
+getNextCounterB :: BlockTransformer Int 
+getNextCounterB = lift getNextCounter
 
 -- Functions for the BlockTransformer Monad --
 
@@ -67,13 +76,33 @@ convert = do convertActAssigns
              convertValAssigns 
              convertRules
 
+-- Go through all of the action assignments, and convert each one into
+--   a block in the IR.  
 convertActAssigns :: Transformer ()
-convertActAssigns = error "Unimplemented" 
+convertActAssigns = do l <- use $ ast . actAssign
+                       mapM_ convertActAssign l
+                                  
+convertActAssign :: P.AAssign -> Transformer() 
+convertActAssign (P.AAssign (P.ID i) b) = 
+  do bID <- convertBlock ("act_assign_" ++ i) b
+     environment %= Map.insert i bID 
+
+convertBlock :: String -> [P.AExpr] -> Transformer ID
+convertBlock s b = do let initState = initBlockState []
+                      retState <- execStateT (mapM_ convertAExpr b) initState
+                      c <- getNextCounter
+                      let id =  s ++ "_" ++ (show c)
+                      let newBlock = retState ^. workingBlock
+                      ir . blocks %= Map.insert (BlockID id) newBlock 
+                      return $ Block id
+
+
+convertAExpr :: P.AExpr -> BlockTransformer ()
+convertAExpr = error "Unimplemented" 
 
 convertValAssigns :: Transformer ()
 convertValAssigns = error "Unimplemented" 
 
 convertRules :: Transformer ()
 convertRules = error "Unimplemented" 
-
 
