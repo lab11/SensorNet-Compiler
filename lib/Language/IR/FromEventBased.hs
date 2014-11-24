5f4442f064eb7aa53b890ea255b830153e8e9630
Rohit Ramesh <Rohit507@gmail.com>
2014-11-24 13:25:58 -0500

    Added preliminary 'convert' and exportable functions

--- lib/Language/IR/FromEventBased.hs
+++ lib/Language/IR/FromEventBased.hs
@@ -63,41 +63,17 @@ fromEventBased p = (execState convert $ initTransformState p) ^. ir
 -- Converter Monads --
 
 convert :: Transformer () 
-convert = error "unimplemented"
+convert = do convertActAssigns 
+             convertValAssigns 
+             convertRules
 
-{-
--- Constructs the inital state for the transformation from an AST
-
--- The one this this module exports the converter function
-  
--- Given a transformer loaded with the correct state this will
---   completely assemble the IR representation  
-transform :: Transformer () 
-transform = do convertActAssigns
-               convertValAssigns
-               convertRules 
-
--- Go through all the elements in an action assignment and make sure 
--- that they are converted to event blocks in the final tree  
 convertActAssigns :: Transformer ()
-convertActAssigns = do x <- get
-                       let a = P.actAssign . ast $ x
-                       if (a /= []) 
-                          then do put (liftAST (P.liftActAssign tail) x)
-                                  addActAssign (head a)
-                                  convertActAssigns 
-                          else return ()
-
-addActAssign :: P.AAssign -> Transformer ()
-addActAssign p = do let P.AAssign id block = p
-                    
-                    return ()
-
---convertBlock :: EventID -> P.Aexpr 
+convertActAssigns = error "Unimplemented" 
 
 convertValAssigns :: Transformer ()
-convertValAssigns = error "unimplemented"
+convertValAssigns = error "Unimplemented" 
 
 convertRules :: Transformer ()
-convertRules = error "unimplemented" 
--}
+convertRules = error "Unimplemented" 
+
+
