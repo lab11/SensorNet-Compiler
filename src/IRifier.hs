7d36b1ce3db59f1699a343d0ec44e5e681aaedb3
Rohit Ramesh <Rohit507@gmail.com>
2014-11-25 17:55:08 -0500

    Fixed module issues.

--- src/IRifier.hs
+++ src/IRifier.hs
@@ -3,6 +3,6 @@ module Main where
 import Text.Show.Pretty
 import Language.EventBased.Lexer
 import Language.EventBased.Parser
-import qualified Language.IR.FromEventBased as IR
+import Language.IR 
 
-main = interact (ppShow . IR.fromEventBased . progParse . tokenize)
+main = interact (ppShow . fromEventBased . progParse . tokenize)
