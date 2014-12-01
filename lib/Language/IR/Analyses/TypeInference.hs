5629464bc585f8728135556f9c49a12b5836c883
David Adrian <davidcadrian@gmail.com>
2014-12-01 10:12:38 -0500

    Start implementing type inference
    http://i.imgur.com/uH8QQ.gif

--- lib/Language/IR/Analyses/TypeInference.hs
+++ lib/Language/IR/Analyses/TypeInference.hs
@@ -1,1 +1,39 @@
-module Language.IR.Analyses.TypeInference
+module Language.IR.Analyses.TypeInference where
+
+import Language.IR
+
+{-}
+data Instruction = SimultAt WaitType Time [BlockID]
+| Store VarID Value
+| Send Email Value
+| Gather TableID [(FieldID,Value)]
+| Call StoReg ExternCall [Value]
+| Concat StoReg [Value]
+| If Value (Maybe BlockID) (Maybe BlockID)
+| BinaryOp StoReg BinOp Value Value
+| UnaryOp StoReg UnOp Value
+deriving (Show,Read,Eq,Ord)
+-}
+
+infer :: Program -> Program
+infer program = Program {
+    _tables = _tables program,
+    _events = _events program,
+    _rules = _rules program,
+    _blocks = fmap inferBlock (getBlockMap program)
+  }
+
+inferBlock :: Block -> Block
+inferBlock block = fmap inferInstruction block
+
+inferInstruction :: Instruction -> Instruction
+{-
+inferInstruction (Store varId value) =
+inferInstruction (Call storeReg externFun args) =
+inferInstruction (Concat storeReg values) =
+inferInstruction (If value _ _) =
+inferInstruction (BinaryOp storeReg op a b) =
+inferInstruction (UnaryOp storeReg op a) =
+-}
+-- Everything else
+inferInstruction s = id s
--- lib/Language/IR/IR.hs
+++ lib/Language/IR/IR.hs
@@ -2,7 +2,7 @@
 
 module Language.IR.IR where
 
-import Data.Map (Map) 
+import Data.Map (Map)
 import qualified Data.Map as Map
 import Language.EventBased.Parser (Interval,Email,BinOp(..),UnOp)
 import Data.Time.LocalTime (LocalTime)
@@ -10,24 +10,24 @@ import Data.List as List
 import Control.Lens
 
 {-
- - Goal of IR design 
+ - Goal of IR design
     - Simplicity for conversion to C
     - Simplicity for analyses
-      - Type Checking / Inference 
+      - Type Checking / Inference
         - TODO : Get the Language.C libraries working so we can get type
                  info from header files.
-      - Power Consumption Estimate 
-      - Memory Use Bounds 
+      - Power Consumption Estimate
+      - Memory Use Bounds
       - Infinite Loop Prevention unless timer spaced
-      - Find upper bound on number of timers/sems needed 
-    - Flatten Structure 
-    - Preserve Semantics 
-      - incl. async gather acts. 
-      - Arbitrarily complex gather acts 
+      - Find upper bound on number of timers/sems needed
+    - Flatten Structure
+    - Preserve Semantics
+      - incl. async gather acts.
+      - Arbitrarily complex gather acts
     - Move to something closer to executable code
     - Make it easier to collate information (table entries, local variables,
        external calls, etc..)
-    - Support future additions to the language with minimal changes 
+    - Support future additions to the language with minimal changes
  -}
 
 newtype BlockID = BlockID String  -- Type Safe Block Identifier
@@ -47,11 +47,11 @@ newtype VarID = VarID String      -- Global Variable Identifier
 
 data Literal = Str String         -- Literal Values
              | Flt Float
-             | Int Int 
-             | Bool Bool 
+             | Int Int
+             | Bool Bool
              deriving (Show,Read,Eq,Ord)
 
-data StoReg = Register RegID      -- Registers we can store into, null just 
+data StoReg = Register RegID      -- Registers we can store into, null just
             | Null                --  discards the value.
             deriving (Show,Read,Eq,Ord)
 
@@ -60,12 +60,12 @@ data Value = Reg RegID            -- Values we can use as parameters
            | Var VarID
            deriving (Show,Read,Eq,Ord)
 
-newtype ExternCall = ExternCall String  -- TypeSafe External Call Identifier 
+newtype ExternCall = ExternCall String  -- TypeSafe External Call Identifier
                    deriving (Show,Read,Eq,Ord)
 
 data Time = Rel Interval
           | Abs LocalTime
-          | Now             
+          | Now
           deriving (Show,Read,Eq,Ord)
 
 data Event = Boot                 -- Default system events (i.e not invoked by
@@ -80,54 +80,54 @@ data WaitType = Waiting
 data Instruction = SimultAt WaitType Time [BlockID]
                  | Store VarID Value
                  | Send Email Value
-                 | Gather TableID [(FieldID,Value)] 
+                 | Gather TableID [(FieldID,Value)]
                  | Call StoReg ExternCall [Value]
-                 | Concat StoReg [Value] 
+                 | Concat StoReg [Value]
                  | If Value (Maybe BlockID) (Maybe BlockID)
                  | BinaryOp StoReg BinOp Value Value
                  | UnaryOp StoReg UnOp Value
                  deriving (Show,Read,Eq,Ord)
-         
+
 type Block = [Instruction]
 
 data Program = Program {
   _tables :: Map TableID [FieldID],
   _events :: Map Event EventID,
-  _rules :: Map EventID [BlockID], 
+  _rules :: Map EventID [BlockID],
   _blocks :: Map BlockID Block
 } deriving (Show,Read,Eq)
 
-emptyProg = Program Map.empty Map.empty Map.empty Map.empty 
+emptyProg = Program Map.empty Map.empty Map.empty Map.empty
 
 makeLenses ''Program
 
 {-
- 
+
  There's a number of invariants that we need to preserve
-  
- - No RegID may be repeated in a block 
+
+ - No RegID may be repeated in a block
  - No RegID may be used before it is defined in a block
- - No RegID may be assigned more than once. 
- - Simult and If must have at least one block to execute, 
+ - No RegID may be assigned more than once.
+ - Simult and If must have at least one block to execute,
  - Never use the String_Append binop
- - All variables must be initialized before they are used. 
+ - All variables must be initialized before they are used.
    i.e. Variables must be initialized in blocks called from Boot without any
         delay.
  - Optional: Register names musn't be repeated across the whole program.
  - Optional: Don't do anything that would require constant propagation
 
- TODO : Write a "Program -> Bool" validity check function that makes sure 
+ TODO : Write a "Program -> Bool" validity check function that makes sure
         none of the above conditions are broken.
 
- TODO : Make sure we can Print the a Programin some human readable way
+ TODO : Make sure we can Print the a Program in some human readable way
 
 
- -} 
+ -}
 
 -- INVARIANT PRESERVATION
 
 programValid :: Program -> Bool
-programValid prog = not 
+programValid prog = not
           ( checkRegNoRepeat prog
          || checkRegDefined prog
          || checkRegNoReassign prog
@@ -137,7 +137,7 @@ programValid prog = not
 
 -- Block Access
 
-getBlockMap :: Program -> Map BlockID Block 
+getBlockMap :: Program -> Map BlockID Block
 getBlockMap = view blocks
 
 getBlocks :: Program -> [Block]
@@ -185,7 +185,7 @@ getSimultIfBlocks (If _ (Just b1) (Just b2)) = [b1,b2]
 getSimultIfBlocks (If _ (Just b1) Nothing) = [b1]
 getSimultIfBlocks (If _ Nothing (Just b2)) = [b2]
 getSimultIfBlocks (If _ Nothing Nothing) = []
-getSimultIfBlocks x = [BlockID ""] 
+getSimultIfBlocks x = [BlockID ""]
 
 getStoreVar :: Instruction -> VarID
 getStoreVar (Store v _) = v
@@ -208,7 +208,7 @@ checkRegDefined :: Program -> Bool    -- No RegID may be used without being defi
 checkRegDefined prog = False `elem` [c `elem` (fst a) | a<-getBlockValRegs prog,b<-(snd a),c<-b]
 
 checkRegNoReassign :: Program -> Bool -- No RegID may be assigned more than once
-checkRegNoReassign prog = let c = [b | a<-(getBlockRegs prog),b<-a] in (List.nub c)/=c 
+checkRegNoReassign prog = let c = [b | a<-(getBlockRegs prog),b<-a] in (List.nub c)/=c
 
 checkSimultIfBlock :: Program -> Bool -- Simult and If must have at least one block to execute
 checkSimultIfBlock prog = 0 `elem` [length (getSimultIfBlocks b) | a<-(getBlocks prog), b<-a]
