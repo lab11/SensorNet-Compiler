98b217d3a733c0e7b2d64b86bb91c13fcac7fd0b
Thomas Zachariah <tzachari@umich.edu>
2014-12-07 20:17:20 -0500

    Set up CFGrapher executable

--- src/IRifier.hs
+++ src/IRifier.hs
@@ -4,6 +4,5 @@ import Text.Show.Pretty
 import Language.EventBased.Lexer
 import Language.EventBased.Parser
 import Language.IR
-import Language.IR.Analyses.TypeInference
 
 main = interact (ppShow . fromEventBased . progParse . tokenize)
