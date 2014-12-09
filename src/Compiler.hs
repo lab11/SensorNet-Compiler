0350775aea787433612609b60397cb0448008a25
Rohit Ramesh <Rohit507@gmail.com>
2014-12-09 15:23:08 -0500

    Compiler works for trivial examples.

---
+++ src/Compiler.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Text.Show.Pretty
+import Language.EventBased.Lexer
+import Language.EventBased.Parser
+import qualified Data.Map as Map
+import Language.IR
+import Language.IR.Analyses.TypeInference
+import Language.IR.ToC
+import System.Environment   
+import System.IO
+
+main = 
+  do args <- getArgs
+     let hf = args !! 0
+     let pf = args !! 1
+     headers <- parseHeaderFile hf 
+     let funcEnv = genFEnv headers
+     progText <- readFile pf 
+     let prog  = (fromEventBased . progParse . tokenize) progText
+     let types = inferTypes prog funcEnv
+     let compiled = toC funcEnv (Map.map head types) prog
+     putStrLn compiled
+
